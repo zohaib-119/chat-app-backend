b34bcf065da24ae4c3a9f826953bd99150fb852b
Muhammad-Zohaib6 <zohaibmian119@gmail.com>
2025-02-22 12:43:08 +0500

    _

--- models/messageReact.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const MessageReactSchema = new mongoose.Schema({
-    group_message_id: { type: mongoose.Schema.Types.ObjectId, ref: "GroupMessage", required: true },
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    react: { type: String, enum: ["like", "love", "haha", "wow", "sad", "angry"], required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model("MessageReact", MessageReactSchema);
