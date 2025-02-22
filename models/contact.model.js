8eb78e07b52abe9dcd07daabe0c3642554eacc07
Muhammad-Zohaib6 <zohaibmian119@gmail.com>
2025-02-22 11:16:50 +0500

    Remove the contact model

--- models/contact.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const ContactSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    contact_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Contact", ContactSchema);
