6e01d1574a835bd8a1401bfda71d07ebad538bb2
Muhammad-Zohaib6 <zohaibmian119@gmail.com>
2025-02-26 23:39:40 +0500

    Implement the chat page with api integrations

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,25 +8,25 @@ const getChatUsers = async (req, res) => {
         // Get all the users with whom the logged in user has chatted
         const chatUsers = await Message.aggregate([
             {
-                $match: { 
-                    $or: [{ sender_id: userId }, { receiver_id: userId }] 
+                $match: {
+                    $or: [{ sender_id: userId }, { receiver_id: userId }]
                 }
             },
             {
                 $group: {
                     _id: null,
-                    uniqueUsers: { 
-                        $addToSet: "$sender_id" 
+                    uniqueUsers: {
+                        $addToSet: "$sender_id"
                     },
-                    uniqueReceivers: { 
-                        $addToSet: "$receiver_id" 
+                    uniqueReceivers: {
+                        $addToSet: "$receiver_id"
                     }
                 }
             },
             {
                 $project: {
-                    allUsers: { 
-                        $setUnion: ["$uniqueUsers", "$uniqueReceivers"] 
+                    allUsers: {
+                        $setUnion: ["$uniqueUsers", "$uniqueReceivers"]
                     }
                 }
             }
@@ -43,9 +43,29 @@ const getChatUsers = async (req, res) => {
 
         res.status(200).json({ success: true, message: 'Chats fetched successfully', chatUsers: users, unseenChats, currentChats: userIds });
     } catch (error) {
-        console.log("Error in checkAuth: ", error.message);
+        console.log("Error in getChatUsers: ", error.message);
         res.status(500).json({ success: false, message: "Internal server error" });
     }
 };
 
-module.exports = { getChatUsers };
+const searchUsers = async (req, res) => {
+    try {
+        console.log('hit')
+        const { username } = req.query;
+        const userId = req.user._id;
+        if (!username) {
+            return res.status(400).json({ message: "Username parameter is required" });
+        }
+
+        const users = await User.find({ username: { $regex: username, $options: "i" }, _id: { $ne: userId } })
+            .limit(5)
+            .select("_id username profile_pic");
+
+        res.status(200).json({ success: true, message: 'Users fetched successfully', users });
+    } catch (error) {
+        console.log("Error in searchUsers: ", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
+
+module.exports = { getChatUsers, searchUsers };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getChatUsers } = require("../controllers/user.controller");
+const { getChatUsers, searchUsers } = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.get("/get-chats",authMiddleware, getChatUsers);
+router.get("/search",authMiddleware, searchUsers);
 
 module.exports = router;
